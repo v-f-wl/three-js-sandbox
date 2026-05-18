27cc931527ad8fc9b98a7b7eacde613c96ecae58
Kim Valentin <co.unicorno@mail.ru>
2026-05-18 23:56:22 +0300

    /

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,27 @@ import { OrbitControls } from '@react-three/drei'
 import './App.css'
 import { Canvas } from '@react-three/fiber'
 
+
+const Cube = ({position, size, color}) => {
+  return(
+    <mesh position={position}>
+      <boxGeometry ards={size}/>
+      <meshStandardMaterial color={color}/>
+    </mesh>
+  )
+}
 function App() {
   return (
     <Canvas fallback={<div>Sorry no WebGL supported!</div>}>
       <directionalLight position={[0, 0, 2]}/>
-      <mesh position={[1, 0, 0]}>
-        <boxGeometry />
-        <meshStandardMaterial color={'orange'}/>
-      </mesh>
+      <ambientLight/>
+
+      <group position={[0, -1, 0]}>
+        <Cube position={[1, 0, 0]} color={'green'} size={[1, 1, 1]}/>
+        <Cube position={[-1, 0, 0]} color={'hotpink'} size={[1, 1, 1]}/>
+        <Cube position={[-1, 2, 0]} color={'blue'} size={[1, 1, 1]}/>
+        <Cube position={[1, 2, 0]} color={'yellow'} size={[1, 1, 1]}/>
+      </group>
     </Canvas>
   )
 }
